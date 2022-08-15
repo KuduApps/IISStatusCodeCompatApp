689bb8e22ea426b125b4a3c36bf27c0bdef5479b
Chris Rosenblatt <crosenblatt@microsoft.com>
2022-08-15 14:34:01 -0700

    Deploy

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.SessionState;
+
+namespace Server462.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        public HttpResponseMessage Get(int id)
+        {
+
+            var res = new HttpResponseMessage();
+            res.StatusCode = HttpStatusCode.OK;
+            res.Content = new StringContent("content!");
+
+            return res;
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Server462.WebApiApplication" Language="C#" %>
